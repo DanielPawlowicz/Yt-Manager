a35a1b90079cf165b470c0a04c4a318f8adcf2de
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-09 19:01:30 +0200

    Backend initialized

---
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/YtManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Daniel.YtManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtManagerBackendApplication.class, args);
+	}
+
+}
---
+++ Yt-Manager-Backend/src/test/java/com/Daniel/YtManagerBackend/YtManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Daniel.YtManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
