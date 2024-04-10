dcfce7e113d1ead51a17750fd7422e6ab1fb4abb
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-10 11:31:17 +0200

    VideoPlaylist entity class

--- Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/VideoPlaylist.java
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/VideoPlaylist.java
@@ -1,4 +1,53 @@
 package com.Daniel.YtManagerBackend.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "video_playlist")
 public class VideoPlaylist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "video_id")
+    private Video video;
+
+    @ManyToOne
+    @JoinColumn(name = "playlist_id")
+    private Playlist playlist;
+
+    private int orderIndex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public int getOrderIndex() {
+        return orderIndex;
+    }
+
+    public void setOrderIndex(int orderIndex) {
+        this.orderIndex = orderIndex;
+    }
 }
