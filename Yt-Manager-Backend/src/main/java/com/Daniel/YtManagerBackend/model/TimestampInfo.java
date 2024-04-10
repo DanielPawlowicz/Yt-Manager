9887a610bc5041c146946a9a244bcdc2bcbc78fe
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-10 18:50:10 +0200

    Timestamp class

---
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/TimestampInfo.java
@@ -0,0 +1,33 @@
+package com.Daniel.YtManagerBackend.model;
+import jakarta.persistence.*;
+
+@Embeddable
+public class TimestampInfo {
+    private String description;
+    private String startTime;
+    private String endTime;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
