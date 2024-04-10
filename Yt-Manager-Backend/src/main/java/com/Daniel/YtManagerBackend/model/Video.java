f65ee225625fbe342ef43ed4316e10191a249ff5
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-10 11:28:17 +0200

    Video entity class

--- Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/Video.java
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/Video.java
@@ -1,4 +1,55 @@
 package com.Daniel.YtManagerBackend.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "video")
 public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String link;
+    private String duration;
+    private String thumbnailUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
 }
