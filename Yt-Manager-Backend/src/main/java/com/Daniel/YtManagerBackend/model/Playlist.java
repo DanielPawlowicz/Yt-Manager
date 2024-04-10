9db8dcadf988ff270b023bb5785fee4511accbd8
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-10 11:29:53 +0200

    Playlist entity class

--- Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/Playlist.java
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/model/Playlist.java
@@ -1,4 +1,28 @@
 package com.Daniel.YtManagerBackend.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "playlist")
 public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String playlistName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
 }
