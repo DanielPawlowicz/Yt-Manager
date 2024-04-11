643f124bd081fd32e2dd454dbfa723b0af709eaf
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-04-11 16:02:32 +0200

    Playlist basic crud operations

--- Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/controller/PlaylistController.java
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/controller/PlaylistController.java
@@ -1,10 +1,13 @@
 package com.Daniel.YtManagerBackend.controller;
 
 import com.Daniel.YtManagerBackend.model.Playlist;
+import com.Daniel.YtManagerBackend.model.Video;
 import com.Daniel.YtManagerBackend.service.PlaylistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -17,9 +20,33 @@ public class PlaylistController {
     }
 
     // create playlist
-    @PostMapping
+    @PostMapping("/add")
     public ResponseEntity<Playlist> createPlaylist(@RequestBody Playlist playlist) {
         Playlist savedPlaylist = playlistService.savePlaylist(playlist);
         return new ResponseEntity<>(savedPlaylist, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Playlist>> getAllPlaylists() {
+        List<Playlist> playlists = playlistService.getAllPlaylists();
+        return ResponseEntity.ok(playlists);
+    }
+
+    @GetMapping("/one/{playlistId}")
+    public ResponseEntity<Playlist> getPlaylistById(@PathVariable Long playlistId) {
+        Playlist playlist = playlistService.getPlaylistById(playlistId);
+        return ResponseEntity.ok(playlist);
+    }
+
+    @PutMapping("/edit/{playlistId}")
+    public ResponseEntity<Playlist> updatePlaylist(@PathVariable Long playlistId, @RequestBody Playlist Playlist){
+        Playlist updatedPlaylist = playlistService.updatePlaylist(playlistId, Playlist);
+        return ResponseEntity.ok(updatedPlaylist);
+    }
+
+    @DeleteMapping("/delete/{playlistId}")
+    public ResponseEntity<Void> deletePlaylist(@PathVariable Long playlistId) {
+        playlistService.deletePlaylist(playlistId);
+        return ResponseEntity.noContent().build();
     }
 }
--- Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/service/PlaylistService.java
+++ Yt-Manager-Backend/src/main/java/com/Daniel/YtManagerBackend/service/PlaylistService.java
@@ -1,8 +1,11 @@
 package com.Daniel.YtManagerBackend.service;
 
+import com.Daniel.YtManagerBackend.controller.exception.NotFoundException;
 import com.Daniel.YtManagerBackend.model.Playlist;
 import com.Daniel.YtManagerBackend.repository.PlaylistRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlaylistService {
@@ -17,5 +20,29 @@ public class PlaylistService {
         return playlistRepository.save(playlist);
     }
 
+    // get all playlists
+    public List<Playlist> getAllPlaylists() {
+        return playlistRepository.findAll();
+    }
+
+    // get specific playlist by id
+    public Playlist getPlaylistById(Long playlistId) {
+        return playlistRepository.findById(playlistId)
+                .orElseThrow(() -> new NotFoundException("Playlist not found with ID: " + playlistId));
+    }
+
+    // update playlist
+    public Playlist updatePlaylist(Long playlistId, Playlist playlist) {
+        Playlist existingPlaylist = playlistRepository.findById(playlistId)
+                .orElseThrow(() -> new NotFoundException("Playlist not found with ID: " + playlistId));
+        existingPlaylist.setPlaylistName(playlist.getPlaylistName());
+
+        return playlistRepository.save(existingPlaylist);
+    }
+
+    // delete playlist
+    public void deletePlaylist(Long playlistId) {
+        playlistRepository.deleteById(playlistId);
+    }
 
 }
